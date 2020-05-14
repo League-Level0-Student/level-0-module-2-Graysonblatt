d474ecfeda37e48da8615fd3e61f4b5d871c415d
root <root@89d32381f12b>
2020-05-14 22:28:48 +0000

    8ball

--- src/_01_random/_4_validation/Validation.java
+++ src/_01_random/_4_validation/Validation.java
@@ -17,9 +17,21 @@ public class Validation {
 		System.out.println(randomNumber);
 
 		// 1. Use each value of randomNumber to give the user a random compliment.
-
+if (randomNumber ==0) {
+JOptionPane.showMessageDialog(null, "You are nice.");
+}
 		// 2. Repeat all the code above 10 times
-		
+if (randomNumber ==1) {
+		JOptionPane.showMessageDialog(null, "You are funny.");
+	}if (randomNumber ==2) {
+		JOptionPane.showMessageDialog(null, "You are weird.(in a good way)");
+	}if (randomNumber ==3) {
+		JOptionPane.showMessageDialog(null, "You are mean.(in a nice way)");
+	}if (randomNumber ==4) {
+		JOptionPane.showMessageDialog(null, "You are smart.");
+	}if (randomNumber ==5) {
+		JOptionPane.showMessageDialog(null, "You are awesome.");
+	}		
 		// 3. Find someone to test out your program. They will like it :)
 	}
 }
